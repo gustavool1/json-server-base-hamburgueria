a82f2b53b2ceeb6cacf6c1450978568b8a6d9cb9
gustavool1 <go8895806@gmail.com>
2023-10-04 15:13:53 -0300

    feat: adding menu route

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
 const port = process.env.PORT || 3001;
-const routes = require('./routes.json');
+const routes = require("./routes.json");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 app.db = router.db;
 
-const rules = auth.rewriter(routes);
+const rules = auth.rewriter({
+  users: 600,
+  products: 444,
+  menu: 644,
+});
 
 app.use(cors());
 app.use(rules);
